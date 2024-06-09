038e816cc48aafd350f4945007ffdfb1d9c0f495
haley <huanghongweimax@163.com>
2024-06-09 23:54:48 +0800

    添加职工信息

--- Headers/workerManager.h
+++ Headers/workerManager.h
@@ -5,6 +5,7 @@
 
 #pragma once   //防止头文件重复包含
 #include "iostream"
+#include "Headers/worker.h"
 
 using namespace std;
 
@@ -20,7 +21,14 @@ public:
     void ExitSystem();
 
 
+
+
     ~WorkerManager();
+
+public:
+    int m_EmpNum; //职工人数
+
+    Worker ** m_EmpArray;  //记录职工数组指针
 };
 
 WorkerManager::WorkerManager() {
@@ -44,4 +52,11 @@ void WorkerManager::Show_Menu()
     cout << "*************  7.清空所有文档  *************" << endl;
     cout << "********************************************" << endl;
     cout << endl;
+}
+
+WorkerManager::WorkerManager() {
+    //初始化属性
+    this->m_EmpNum = 0;
+
+    this->m_EmpArray = NULL;
 }
