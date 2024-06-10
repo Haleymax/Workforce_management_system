130ae40fa795221433587236d9a97bdda441fd65
haley <huanghongweimax@163.com>
2024-06-10 11:39:03 +0800

    添加职工信息

--- Headers/workerManager.h
+++ Headers/workerManager.h
@@ -5,7 +5,10 @@
 
 #pragma once   //防止头文件重复包含
 #include "iostream"
-#include "Headers/worker.h"
+#include "worker.h"
+#include "employee.h"
+#include "manager.h"
+#include "boss.h"
 
 using namespace std;
 
@@ -20,6 +23,9 @@ public:
     //退出系统
     void ExitSystem();
 
+    //添加职工
+    void Add_Emp();
+
 
 
 
@@ -32,11 +38,16 @@ public:
 };
 
 WorkerManager::WorkerManager() {
+    //初始化属性
+    this->m_EmpNum = 0;
 
+    //初始化数组指针
+    this->m_EmpArray = NULL;
 }
 
 WorkerManager::~WorkerManager() {
-
+    if (this->m_EmpArray != NULL)
+        delete[] this->m_EmpArray;
 }
 void WorkerManager::Show_Menu()
 {
@@ -54,9 +65,81 @@ void WorkerManager::Show_Menu()
     cout << endl;
 }
 
-WorkerManager::WorkerManager() {
-    //初始化属性
-    this->m_EmpNum = 0;
 
-    this->m_EmpArray = NULL;
+//添加职工的实现
+void WorkerManager::Add_Emp() {
+    cout << "请输入增加的职工数量" << endl;
+
+    int addNum = 0;
+    cin >> addNum;
+
+    if (addNum > 0){
+        //采用可变长数组的形式放入新职工，新开辟一个数组来存放替代原来的旧数组
+
+        //计算新空间的大小
+        int newSize = this->m_EmpNum + addNum;
+
+        //开辟新空间
+        Worker ** newSpace = new Worker * [newSize];
+
+        //将原空间下内容存放到新空间下
+        if (this->m_EmpNum != NULL){
+            for (int i = 0; i < this->m_EmpNum; ++i) {
+                newSpace[i] = this->m_EmpArray[i];
+            }
+        }
+
+        //输入新数据
+        for (int i = 0; i < addNum; ++i) {
+            int id;
+            string name;
+            int dSelect;
+
+            cout<< "请输入第 " << i + i << " 个新职工编号 ：" <<endl;
+            cin >> id;
+
+            cout<< "请输入第 " << i + 1 << "个新职工姓名 : " <<endl;
+            cin >> name;
+
+            cout << "请选择g该职工的岗位 : " << endl;
+            cout << "1、普通职工" <<endl;
+            cout << "2、经理" << endl;
+            cout << "3、老板" << endl;
+            cin >> dSelect;
+
+            //创建新职工
+            Worker *worker = NULL;
+            switch (dSelect) {
+                case 1:   //普通职工
+                    worker = new Employee(id,name,1);
+                    break;
+                case 2: //经理
+                    worker = new Manager(id,name,2);
+                    break;
+                case 3: //老板
+                    worker = new Boss(id,name,3);
+                    break;
+                default:
+                    break;
+            }
+
+            newSpace[this->m_EmpNum + i] = worker;
+        }
+        //释放原有空间
+        delete[] this->m_EmpArray;
+
+        //管理系统的职工信息数组指向新开的数组中
+        this->m_EmpArray = newSpace;
+
+        //更新职工数量
+        this->m_EmpNum = newSize;
+
+        //提示添加成功
+        cout << "成功添加" << addNum << "名新职工！" << endl;
+    } else{
+        cout << "输入有误" <<endl;
+    }
+
+    system("pause");
+    system("cls");
 }
